c79c26a0e943557df188b5abde60d28a95511ca1
Abdulaziz2003333 <lboy21044@gmail.com>
2025-10-06 14:04:42 +0500

    add journal files

--- features/login/scheme/login-scheme.ts
+++ features/login/scheme/login-scheme.ts
@@ -4,7 +4,7 @@ export const loginScheme = z.object({
   username: z.string().min(3, {
     message: "Foydalanuvchi nomi 3 tadan katta bo'lishi kerak",
   }),
-  password: z.string().min(8, {
+  password: z.string().min(3, {
     message: "Parol uzunligi 8 tadan katta bo'lishi kerak",
   }),
 });
